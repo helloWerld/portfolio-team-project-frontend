80ab812eeb6dc9c6e9116d51d1569034a4370f54
SolPyDev <108149462+SolPyDev@users.noreply.github.com>
2023-04-21 11:09:32 -0400

    remove border on map

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
           <buttom type='submit' className='bg-gradient-to-r from-indigo-600 via-purple-600 to-rose-600 text-black text-lg px-3 py-2 w-fit text-center rounded-lg cursor-pointer hover:scale-105 hover:shadow-lg hover:shadow-indigo-600/30'>Submit</buttom>
         </form>
       </div>
-      <div className='w-full h-1/2 lg:w-1/2 lg:h-screen border-4 absolute bottom-0 left-0 right-0 lg:right-0 lg:left-auto'>
+      <div className='w-full h-1/2 lg:w-1/2 lg:h-screen absolute bottom-0 left-0 right-0 lg:right-0 lg:left-auto'>
         <iframe className='w-full h-full lg:h-screen' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d114018.46213880564!2d-80.19925084648446!3d26.74192066317234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d8d5ccb595adc1%3A0x15efc7b51fe16bde!2sWest%20Palm%20Beach%2C%20FL!5e0!3m2!1sen!2sus!4v1681157390472!5m2!1sen!2sus"></iframe>
       </div>
     </div>
